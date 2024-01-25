42ebe74979bde22dfef84b515fc5f7842e2e4ef9
Sofya02 <nikolaenko.sofya@mail.ru>
2024-01-25 17:58:20 +0300

    changes in working_console.cs

--- working_console.cs
+++ working_console.cs
@@ -1,1 +1,54 @@
 // Классы/функции для работы с консолью (ввод/вывод базовых типов данных)
+
+Console.Write("Enter the string: ");
+string userInput = Console.ReadLine();
+
+Console.Write("Enter an integer: ");
+int intInput = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter a floating point number: ");
+double doubleInput = Convert.ToDouble(Console.ReadLine());
+
+
+string outputString = "Hello, world!";
+Console.WriteLine(outputString);
+
+string name = "John";
+int age = 25;
+Console.WriteLine($"Name: {name}, Age: {age}");
+
+int result = 42;
+Console.WriteLine("Result: " + result);
+
+
+int num = 10;
+string str = "Hello, world!";
+Console.WriteLine("The number is: " + num);
+Console.WriteLine("The string is: " + str);
+
+Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name + "!");
+
+int age = 25;
+Console.Write("Your age is: ");
+Console.Write(age);
+
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+Console.WriteLine("Key pressed!");
+
+Console.WriteLine("Enter a number:");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+Console.WriteLine("The square of the number is: " + (number * number));
+
+Console.WriteLine("Enter a decimal number:");
+string input = Console.ReadLine();
+double number = Convert.ToDouble(input);
+Console.WriteLine("The square root of the number is: " + Math.Sqrt(number));
+
+Console.WriteLine("Press any key to clear the console...");
+Console.ReadKey();
+Console.Clear();
+Console.WriteLine("Console cleared!");
