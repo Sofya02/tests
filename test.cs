b77bcf4d3b502688df83a2d32bb4db6e663079ee
Sofya02 <nikolaenko.sofya@mail.ru>
2024-01-29 10:22:49 +0300

    en

--- test.cs
+++ test.cs
@@ -2,69 +2,69 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        // 1. Сортировка массива + ввод/вывод с клавиатуры
-        int[] numbers = new int[5];
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            Console.Write("Введите число: ");
-            numbers[i] = Convert.ToInt32(Console.ReadLine());
-        }
+     static void Main(string[] args)
+     {
+         // 1. Array sorting + keyboard input/output
+         int[] numbers = new int[5];
+         for (int i = 0; i < numbers.Length; i++)
+         {
+             Console.Write("Enter a number: ");
+             numbers[i] = Convert.ToInt32(Console.ReadLine());
+         }
 
-        Array.Sort(numbers);
+         Array.Sort(numbers);
 
-        Console.WriteLine("Отсортированный массив:");
-        foreach (int number in numbers)
-        {
-            Console.WriteLine(number);
-        }
+         Console.WriteLine("Sorted array:");
+         foreach (int number in numbers)
+         {
+             Console.WriteLine(number);
+         }
 
-        // 2. Рекурсивная функция (числа Фибоначчи)
-        int n = 10;
-        for (int i = 0; i < n; i++)
-        {
-            Console.WriteLine(Fibonacci(i));
-        }
+         // 2. Recursive function (Fibonacci numbers)
+         int n = 10;
+         for (int i = 0; i < n; i++)
+         {
+             Console.WriteLine(Fibonacci(i));
+         }
 
-        // 3. Передача параметров в функцию по ссылке/значению
-        int a = 10;
-        ChangeValue(a); // передача по значению
-        Console.WriteLine(a); // выведет 10
+         // 3. Passing parameters to a function by reference/value
+         int a = 10;
+         ChangeValue(a); // passing by value
+         Console.WriteLine(a); // will print 10
 
-        ChangeValue(ref a); // передача по ссылке
-        Console.WriteLine(a); // выведет 20
+         ChangeValue(ref a); // passing by reference
+         Console.WriteLine(a); // will print 20
 
-        // 4. Сверка с референсом
-        string str1 = "Hello";
-        string str2 = "Hello";
+         // 4. Verification with reference
+         string str1 = "Hello";
+         string str2 = "Hello";
 
-        Console.WriteLine(ReferenceEquals(str1, str2)); // выведет True
+         Console.WriteLine(ReferenceEquals(str1, str2)); // will print True
 
-        // 5. В логических операциях деление на 0 и exception
-        int numerator = 10;
-        int denominator = 0;
+         // 5. In logical operations, division by 0 and exception
+         int numerator = 10;
+         int denominator = 0;
 
-        bool result = denominator != 0 && numerator / denominator > 0;
+         bool result = denominator != 0 && numerator / denominator > 0;
 
-        Console.WriteLine(result); // выведет False
-    }
+         Console.WriteLine(result); // will print False
+     }
 
-    static int Fibonacci(int n)
-    {
-        if (n <= 1)
-            return n;
-        else
-            return Fibonacci(n - 1) + Fibonacci(n - 2);
-    }
+     static int Fibonacci(int n)
+     {
+         if (n <= 1)
+             return n;
+         else
+             return Fibonacci(n - 1) + Fibonacci(n - 2);
+     }
 
-    static void ChangeValue(int x)
-    {
-        x += 10;
-    }
+     static void ChangeValue(int x)
+     {
+         x += 10;
+     }
 
-    static void ChangeValue(ref int x)
-    {
-        x += 10;
-    }
+     static void ChangeValue(ref int x)
+     {
+         x += 10;
+     }
 }
